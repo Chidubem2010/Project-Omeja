11185b492a8d6e4d1dd43b3b7cb715a5679eaba0
Dubem <daimianpriest@gmail.com>
2023-10-25 21:21:15 +0100

    first commit

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        setTimeout(carousel, 9000);
+}
+
+let visible = document.getElementById("notes");
+let icon = document.getElementById("icon")
+function showAppointement() {
+    visible.style.display= "block";
+}
+function removAppiontement() {
+    visible.style.display = "none";
+}
